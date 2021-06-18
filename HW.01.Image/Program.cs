9a226b6420184615995da20083389a07f23de2e8
Anton <samantonovich@gmail.com>
2021-06-18 21:45:47 +0300

    HW_02: Add image read code

--- HW.01.Image/Program.cs
+++ HW.01.Image/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace HW._01.Image
 {
@@ -6,7 +7,23 @@ namespace HW._01.Image
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StreamReader textReader = new StreamReader(@"C:\Temp\image.txt", true);
+
+            string textReaderResult = textReader.ReadToEnd();
+
+            textReader.Dispose();
+
+            string[] arrayOfTextResult = textReaderResult.Split(' ');
+
+            byte[] imageBytes = new byte[arrayOfTextResult.Length - 1];
+
+            for (int i = 0; i < arrayOfTextResult.Length - 1; i++)
+            {
+                byte binary = Convert.ToByte(arrayOfTextResult[i], 2);
+                imageBytes[i] = binary;
+            }
+
+            File.WriteAllBytes(@"C:\Temp\image.png", imageBytes);
         }
     }
 }
