72e430827846fdda34b8d3d8795c89e903e8e71e
Anton <samantonovich@gmail.com>
2021-06-18 22:04:40 +0300

    HW_02: Add casting proj

---
+++ HW.03.Casting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW._03.Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
