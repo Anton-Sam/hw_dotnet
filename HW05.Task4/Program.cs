50f3708dd3d85fc38318adc01c09974d222748da
Anton <samantonovich@gmail.com>
2021-06-28 22:30:44 +0300

    HW_05: Add Task 4 (Task 4 proj)

---
+++ HW05.Task4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HW05.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var addr = "1123 Main Street St. Louisville OH 43071,432 Main Long Road St. Louisville OH 43071,786 High Street Pollocksville NY 56432";
+            Console.WriteLine(Travel(addr, "OH 43071"));
+        }
+
+        private static string Travel(string addresses, string zipcode)
+        {
+            if (!Regex.Match(zipcode,@"^[A-Z]{2}\s\d{5}$").Success)
+                return $"{zipcode}:/";
+
+            var addrList = addresses.Split(',').Where(s => s.EndsWith(zipcode));               
+            var houses = new List<string>();
+            var towns = new List<string>();
+
+            foreach (var addr in addrList)
+            {
+                var house = Regex.Match(addr, @"^\d+").Value;
+                houses.Add(house);
+                var town = Regex.Match(addr,$@"(?<={house}\s).+(?=\s{zipcode})").Value;
+                towns.Add(town);
+            }
+
+            return $"{zipcode}:{string.Join(',', towns)}/{string.Join(',', houses)}";
+        }
+    }
+}
