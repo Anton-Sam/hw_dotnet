253c28098f61a3ebb4148c21692fd3f4545603a3
Anton <samantonovich@gmail.com>
2021-07-13 21:44:21 +0300

    HW08: Add Task 1 proj

---
+++ HW08.Task1/Models/ItCompanySalaryService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW08.Task1.Models
+{
+    public static class ItCompanySalaryService
+    {
+        private static List<Engineer> _engineers = new List<Engineer>();
+        public static string Company { get; set; } = "SaM Solutions";
+        
+        public static void AddEngineer(Engineer engineer) => _engineers.Add(engineer);
+
+        public static IEnumerable<Engineer> GetEngineers() => _engineers.OrderBy(eng=>eng.Experience);
+
+        public static void PrintEngineersInfo()=> GetEngineers().ToList().ForEach(eng => Console.WriteLine($"Company: {Company},{eng}"));
+    }
+}
---
+++ HW08.Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using HW08.Task1.Models;
+using System;
+using System.Collections.Generic;
+
+namespace HW08.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Engineer> engineers = new List<Engineer>
+            {
+                new TeamLeader("Artur","Rutra",7,"Manage","https://github.com/RutArt"),
+                new Junior("Anton", "Samovich", 1, "Fix bags", "https://github.com/Anton-Sam"),
+                new Senior("Vasya","Zaicev",5,"Develop and manage","https://github.com/ZaiVAS"),
+                new Middle("Ivan", "Ivanov", 3, "Develop and support", "https://github.com/IIvanov"),
+                new Architect("Vadzim","Papko",9,"Create architecture and manage","https://github.com/VadzimPapko")
+            };
+            engineers.ForEach(ItCompanySalaryService.AddEngineer);
+            ItCompanySalaryService.PrintEngineersInfo();
+        }
+    }
+}
